f8cb696b361007a8a82604880e64baa25417a53f
Ubuntu <ubuntu@ip-172-31-28-72.ap-northeast-2.compute.internal>
2018-10-08 08:40:49 +0000

    logger 추가

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const { User } = require('../model/user');
+const logger = require('../log');
 
 // GET /users
 router.get('/', async (req, res) => {
@@ -27,6 +28,7 @@ router.post('/', async (req, res) => {
 
     res.status(200).send(createdUser);
   } catch (e) {
+//	logger.error('POST /users : %o', e);
     res.status(400).send(e);
   }
 });
