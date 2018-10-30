f7465844a7ea70ecbf75d8e3cc2abcf199cc51ba
SangWoo <sangw0804@likelion.org>
2018-10-30 16:19:13 +0900

    couponRoute 수정

--- model/coupon.js
+++ model/coupon.js
@@ -31,11 +31,11 @@ const couponSchema = new mongoose.Schema(
   }
 );
 
-couponSchema.statics.makeCoupons = async function(point, number, forDisgner) {
+couponSchema.statics.makeCoupons = async function(point, number, forDesigner) {
   const Coupon = this;
   const coupons = [];
   for (let i = 0; i < number; i++) {
-    coupons.push(await Coupon.create({ point, _id: Math.floor(Math.random() * 10 ** 12), forDisgner }));
+    coupons.push(await Coupon.create({ point, _id: Math.floor(Math.random() * 10 ** 12), forDesigner }));
   }
   return coupons;
 };
