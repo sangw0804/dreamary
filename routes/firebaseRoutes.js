c40d70e3e74bf29be0e8c5cbbae264e7dcdf47cf
SangWoo <sangw0804@likelion.org>
2018-11-21 00:46:14 +0900

    lets sharp

--- routes/firebaseRoutes.js
+++ routes/firebaseRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const AWS = require('aws-sdk');
 const fs = require('fs');
 const firebase = require('firebase');
+const sharp = require('sharp');
 
 const formPromise = require('./helpers/formidablePromise');
 const { Recruit } = require('../model/recruit');
@@ -22,11 +23,15 @@ router.post('/upload', async (req, res) => {
 
     const promises = Object.keys(files).map(async fileType => {
       const s3 = new AWS.S3();
+      const croppedFile = await sharp(files[fileType].path)
+        .rotate()
+        .toFile(`${files[fileType].name}_cropped`);
+      console.log(croppedFile);
       const params = {
         Bucket: 'dreamary',
         Key: randomNum + files[fileType].name,
         ACL: 'public-read',
-        Body: fs.createReadStream(files[fileType].path)
+        Body: croppedFile
       };
 
       const data = await s3.upload(params).promise();
