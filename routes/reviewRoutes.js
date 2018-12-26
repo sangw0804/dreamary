4973dcfeec0d6a62b32119d14efa0b9bf43ca5ad
SangWoo <sangw0804@likelion.org>
2018-12-26 14:36:51 +0900

    review 여러 사진 업로드

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -77,9 +77,11 @@ router.patch('/:id/images', async (req, res) => {
     });
 
     const fileLocations = await Promise.all(promises);
-    if (fileLocations.length) await Review.findByIdAndUpdate(id, { $set: { images: fileLocations } });
 
-    res.status(200).send({});
+    const review = Review.findById(id);
+    review.images = review.images.concat(fileLocations);
+
+    res.status(200).send(review);
   } catch (e) {
     if (logger) logger.error('PATCH /recruits/:recruit_id/reviews/:id/images | %o', e);
     res.status(400).send(e);
