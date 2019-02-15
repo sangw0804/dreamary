8bc998e4ff38bf35b6a904d313d3622557acb063
SangWoo <sangw0804@likelion.org>
2019-02-15 17:11:08 +0900

    withdraw 모델 추가

---
+++ model/withdraw.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const withdrawSchema = new mongoose.Schema({
+  _designer: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  name: {
+    type: mongoose.Schema.Types.String,
+    required: true
+  },
+  socialId: {
+    type: mongoose.Schema.Types.String,
+    required: true
+  },
+  address: {
+    type: mongoose.Schema.Types.String,
+    required: true
+  },
+  email: {
+    type: mongoose.Schema.Types.String,
+    required: true
+  },
+  accountHolder: {
+    type: mongoose.Schema.Types.String,
+    required: true
+  },
+  bank: {
+    type: mongoose.Schema.Types.String,
+    required: true
+  },
+  accountNumber: {
+    type: mongoose.Schema.Types.String,
+    required: true
+  },
+  money: {
+    type: mongoose.Schema.Types.Number,
+    required: true
+  }
+});
+
+const Withdraw = mongoose.model('Withdraw', withdrawSchema);
+
+module.exports = { Withdraw };
