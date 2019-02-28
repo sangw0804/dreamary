3ccf529f51ad72ccca4bb68c84a2f32869eeed18
SangWoo <sangw0804@likelion.org>
2019-02-28 13:21:07 +0900

    coupon 주석

--- model/coupon.js
+++ model/coupon.js
@@ -9,29 +9,35 @@ const couponSchema = new mongoose.Schema(
       required: true
     },
     forDesigner: {
+      // 디자이너용 쿠폰/ 유저용 쿠폰 true/false
       type: Boolean,
       required: true
     },
     point: {
+      // 쿠폰의 포인트 가치
       type: Number,
       required: true
     },
     _user: {
+      // 쿠폰을 사용한 유저의 _id값
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
       default: null
     },
     isMaster: {
+      // 마스터 쿠폰 여부
       type: Boolean,
       default: false
     },
     _master_users: [
+      // 마스터 쿠폰을 사용한 유저들의 _id값들
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
       }
     ],
     createdAt: {
+      // 생성 일자
       type: Number,
       required: true
     }
@@ -42,6 +48,7 @@ const couponSchema = new mongoose.Schema(
 );
 
 couponSchema.statics.makeCoupons = async function makeCouponHandler(point, number, forDesigner) {
+  // 주어진 point 가치를 가지는 쿠폰을 number개 만들어서 배열로 리턴.
   const Coupon = this;
   const coupons = [];
   for (let i = 0; i < number; i++) {
@@ -58,6 +65,7 @@ couponSchema.statics.makeCoupons = async function makeCouponHandler(point, numbe
 };
 
 couponSchema.methods.checkMasterUserInclude = function checkHandler(userId) {
+  // 마스터쿠폰을 주어진 _id의 유저가 사용했는지 여부를 반환
   const masterCoupon = this;
   const userIdStrings = masterCoupon._master_users.map(objectId => objectId.toHexString());
   return userIdStrings.includes(userId.toHexString());
